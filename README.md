aa45899f4dfebc5c9eb608e49e3ac28eb0cf5df7
gopye <gopye@tuta.io>
2020-10-27 12:16:11 -0700

    Add env vars and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # twit-rip
 pull all of a users tweets, retweets, and responses
+
+twit-rip is to be ran as script.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let test_var = env::var("TESTVAR").expect("Please set TESTVAR");
+    let test_var2 = env::var("TESTVAR2").expect("Please set TESTVAR2");
+
+    println!("{}", test_var);
+    println!("{}", test_var2);
 }
